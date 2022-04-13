76c3ae171350d5d983beeb1d4a141c31eca065c2
Saiprakash2075 <96859478+Saiprakash2075@users.noreply.github.com>
2022-04-13 23:05:00 +0530

    Create SmallestMissing.Java

---
+++ SmallestMissing.Java
@@ -0,0 +1,29 @@
+package DSAAssg1;
+
+public class Smaletmissng {
+    public static int findSmallestMissing(int[] nums, int left, int right) {
+        // base condition
+        if (left > right) {
+            return left;
+        }
+
+        int mid = left + (right - left) / 2;
+
+        // if the mid-index matches with its value, then the mismatch
+        // lies on the right half
+        if (nums[mid] == mid) {
+            return findSmallestMissing(nums, mid + 1, right);
+        } else {
+            // mismatch lies on the left half
+            return findSmallestMissing(nums, left, mid - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 2, 3, 4, 5, 6};
+
+        int left = 0, right = nums.length - 1;
+        System.out.println("The smallest missing element is "
+                + findSmallestMissing(nums, left, right));
+    }
+}
